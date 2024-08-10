f82e267fb0d6fb55711ba2d976774b84f34bb63b
skc0924 <skc0924@gmail.com>
2024-08-10 11:57:07 +0530

    Code added to files

---
+++ check_even.py
@@ -0,0 +1,5 @@
+def even_check(n):
+    if n%2==0:
+        print("This is even number.")
+    else:
+        print("This is odd number")
---
+++ check_odd.py
@@ -0,0 +1,5 @@
+def odd_check(n):
+    if n%2!=0:
+        print("This is odd number.")
+    else:
+        print("This is even number")
